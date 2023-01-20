5ec860ab4e1cae1a08217e9928bc94b7ba0d8f6f
Fdamirez <fabianramirez0072@gmail.com>
2023-01-20 17:00:02 -0500

    Finish Infrastucture

---
+++ Infrastructure/Common/Response.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class Response<TEntity> where TEntity: DomainEntity
+    {
+        public IEnumerable<TEntity> Entities { get; set; }
+        public string Message { get; set; }
+        public bool Error { get; set; }
+        public TEntity Entity { get; set; }
+
+        public Response(IEnumerable<TEntity> entities)
+        {
+            Entities = entities;
+            Error = false;
+        }
+
+        public Response(TEntity entity)
+        {
+            Entity = entity;
+            Error = false;
+        }
+
+        public Response(string message)
+        {
+            Message = message;
+            Error = true;
+        }
+    }
+}
---
+++ Infrastructure/Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions contextOptions) : base(contextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public async Task CommitAsync()
+        {
+            await SaveChangesAsync().ConfigureAwait(false);
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
